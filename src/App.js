37ae4afe99b0b2b142c83c1ecc4f821114d03964
kevintl1998 <kevintl1998@gmail.com>
2022-11-05 21:22:03 -0400

    added about.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import NavBar from './button';
+import About from './Home/About';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<NavBar/>} />
+          <Route path="/about" element={<About/>} />
       </Routes>
     </Router>
   );
--- src/Home/About.js
+++ src/Home/About.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 function About() {
-    window.location.href = "https://devpost.com/software/example-template-submission";
+    window.setTimeout(window.location.href = "https://devpost.com/software/example-template-submission",5000);
     return (
         <div/>
     )
