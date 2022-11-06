12886c4914db2fcb96e3ccf946fabded73e6a29a
Saheer <saheerahmad1@gmail.com>
2022-11-05 20:00:51 -0500

    home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import Home from './pages/Home';
+import Home from './Home/Home';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
-import Explore from './pages/Explore';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/explore" element={<Explore/>} />
       </Routes>
     </Router>
   );
