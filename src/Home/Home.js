c2976d2040b87e852fd7d9c7c96d2cf0da66ad42
Saheer <saheerahmad1@gmail.com>
2022-11-05 20:52:53 -0500

    update

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,25 +2,35 @@ import React from "react";
 import './Home.css'
 import Logo from '../assets/Logo.png'
 import acc_img from '../assets/account.png'
+import house from '../assets/image.png';
 
 function Home(){
     return(
-        <div>
+        <div className="container">
             <div className="Nav">
                 <div className="rowFlex">
                     <img className="Logo" src={Logo} alt="Logo" />
                     <h2 className="Title">GoodCandy</h2>
                 </div>
 
-                <div className="rowFlex">
-                    <a href="">About</a>
+                <div className="rowFlexMid">
+                    <a href="./about">About</a>
                     <a href="">Find Houses</a>
                 </div>
 
                 <div className="rowFlex">
-                    <a href=""><button>Write a Review</button></a>
+                    <a href="./submit_review"><button>Write a Review</button></a>
                     <img className="account" src={acc_img} alt="account" />
                 </div>
+            </div>
+            
+            <div className="midDiv">
+                <div className="midText">
+                    <h2>Make the most of your Halloween</h2>
+                    <p>Look through thousands of reviews to find the best spots for candy</p>
+                    <a href=""><button>Get Started</button></a>
+                </div>
+                <img src={house} alt="" />
             </div>
         </div>
     );
