b5ea6e008d95b9cc44627c6579193fb6cfe65b9b
kevintl1998 <kevintl1998@gmail.com>
2022-11-06 08:32:38 -0500

    small fixes

--- src/Home/Cards.js
+++ src/Home/Cards.js
@@ -18,18 +18,18 @@ const Cards = (props) => {
     }, []);
 
     const reviewCards = reviews.map((review) => {
-        return (
-            <div className="card">
-                <div className="container">
-                    <img src={hotel} alt="" />
-                </div>
+            return (
+                <div className="card">
+                    <div className="container">
+                        <img src={hotel} alt="" />
+                    </div>
                     <h2>Address: {review.Address}, {review.ZipCode}</h2>
                     <p>Rating: {review.Rating}/5</p>
                     <p>Review: {review.Review}</p>
                     <p>-{review.FullName}</p>
                 </div>
-        )
-    }
+            )
+        }
     )
     return (
         <>
