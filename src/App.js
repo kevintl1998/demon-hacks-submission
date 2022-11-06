1b6d187726a0cd875cd23a9291dc32b4348ce547
kevintl1998 <kevintl1998@gmail.com>
2022-11-05 21:22:56 -0400

    added about.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
+import Home from "./Home/Home";
 import NavBar from './button';
 import About from './Home/About';
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
+          <Route path="/" element={<Home/>} />
         <Route path="/login" element={<NavBar/>} />
           <Route path="/about" element={<About/>} />
       </Routes>
