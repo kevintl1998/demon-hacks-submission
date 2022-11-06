a93fd615da8489898d67d1b3359bba6ca837c500
Rohan Sagar <sagarrohan2002@gmail.com>
2022-11-06 06:43:20 -0600

    Cards update

--- src/Home/Cards.js
+++ src/Home/Cards.js
@@ -1,18 +1,41 @@
 import React from "react";
 import './Cards.css'
 import hotel from "../assets/hotel.png"
+import axios from "axios";
 
-function Cards(props){
-    return(
-        <div className="card">
-            <div className="container">
-                <img src={hotel} alt="" />
-                <h2>{props.name}</h2>
-                <p>{props.rating}</p>
-                <p>{props.review}</p>
-            </div>
-        </div>
-    );
+// call api to get reviews and display them in cards
+const Cards = (props) => {
+    const [reviews, setReviews] = React.useState([]);
+
+    React.useEffect(() => {
+        axios.get('http://localhost:9000/get-reviews')
+            .then((res) => {
+                setReviews(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }, []);
+
+    const reviewCards = reviews.map((review) => {
+        return (
+            <div className="card">
+                <div className="container">
+                    <img src={hotel} alt="" />
+                </div>
+                    <h2>Address: {review.Address}, {review.ZipCode}</h2>
+                    <p>Rating: {review.Rating}</p>
+                    <p>Review: {review.Review}</p>
+                    <p>-{review.FullName}</p>
+                </div>
+        )
+    }
+    )
+    return (
+        <>
+            {reviewCards}
+        </>
+    )
 }
 
 export default Cards;
