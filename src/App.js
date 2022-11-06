b5d61f72dd64234f043ec88bb2494b3d63779aef
Rohan Sagar <sagarrohan2002@gmail.com>
2022-11-06 03:40:50 -0600

    Updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Home from "./Home/Home";
-import NavBar from './button';
 import About from './Home/About';
 import WriteReview from './Home/WriteReview';
 
@@ -10,7 +9,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<NavBar/>} />
         <Route path="/about" element={<About/>} />
         <Route path="/submit_review" element={<WriteReview/>} />
       </Routes>
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -4,8 +4,20 @@ import Logo from '../assets/Logo.png'
 import acc_img from '../assets/account.png'
 import house from '../assets/image.png';
 import MapAndReviewContainer from '../containers/MapAndReviewContainer'
+import { useAuth0 } from "@auth0/auth0-react";
+import { NavLink as RouterNavLink } from "react-router-dom";
 
-function Home(){
+const Home = () => {
+    const {
+        loginWithRedirect,
+        logout,
+        } = useAuth0();
+    
+    const logoutWithRedirect = () =>
+    logout({
+        returnTo: window.location.origin,
+    });
+    
     return(
         <div className="container">
             <div className="Nav">
@@ -19,17 +31,19 @@ function Home(){
                     <a href="">Find Houses</a>
                 </div>
 
+
                 <div className="rowFlexRight">
                     <a href="/submit_review"><button>Write a Review</button></a>
-                    <img className="account" src={acc_img} alt="account" />
+                    <img className="account" onClick={() => logoutWithRedirect()} src={acc_img} alt="account" />
                 </div>
+
             </div>
 
             <div className="midDiv">
                 <div className="midText">
                     <h2>Make the most of your Halloween</h2>
                     <p>Look through thousands of reviews to find the best spots for candy</p>
-                    <a href=""><button>Get Started</button></a>
+                    <a href=""><button onClick={() => loginWithRedirect()}>Get Started</button></a>
                 </div>
                 <img src={house} alt="" />
             </div>
